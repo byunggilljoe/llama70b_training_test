37f68c2e3d76ec17889079cc70bfff6d7965cb74
byunggill <byunggill@gmail.com>
2024-05-10 06:46:21 +0000

    padding max length 2048, OOM at iteration 5

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ from typing import Optional
 import torch
 from transformers import set_seed
 
-from transformers import HfArgumentParser, TrainingArguments
+from transformers import HfArgumentParser, TrainingArguments, DataCollatorWithPadding
 from trl import SFTTrainer
 from utils import (
     create_and_prepare_model,
@@ -186,6 +186,7 @@ def main(model_args, data_args, training_args):
         },
         dataset_text_field=data_args.dataset_text_field,
         max_seq_length=data_args.max_seq_length,
+        data_collator=DataCollatorWithPadding(tokenizer, padding="max_length", max_length=data_args.max_seq_length)
     )
     trainer.accelerator.print(f"{trainer.model}")
     if model_args.use_peft_lora:
