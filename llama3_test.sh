76afcd2f851e104adc4fe439eb560a6e02b4acf3
byunggill <byunggill@gmail.com>
2024-05-10 04:19:02 +0000

    '--mixed_precision bf16' option for 'accelerate launch' made a progress. A single forward pass seems done in LLAMA3 8b training

--- llama3_test.sh
+++ llama3_test.sh
@@ -4,12 +4,13 @@ NUM_MACHINES=1
 MACHINE_RANK=0
 
 accelerate launch \
-    --config_file configs/fsdp_config.yaml \
+    --config_file configs/fsdp_config_llama3.yaml \
     --main_process_ip $MASTER_ADDR \
     --main_process_port $MASTER_PORT \
     --machine_rank $MACHINE_RANK \
     --num_processes 8 \
     --num_machines ${NUM_MACHINES} \
+    --mixed_precision bf16 \
     train.py \
     --seed 100 \
     --model_name "meta-llama/Meta-Llama-3-70B" \
@@ -34,7 +35,7 @@ accelerate launch \
     --weight_decay 0.01 \
     --warmup_ratio 0.03 \
     --max_grad_norm 1.0 \
-    --output_dir "./shared_storage/sourab/experiments/full-finetune-llama-chat-asst" \
+    --output_dir "./shared_storage/sourab/experiments/Meta-Llama-3-70B-asst" \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
